2ec1a3d8e0abd011681efdf2fe89145f8be52b4f
Will Chen <wfchen1990@gmail.com>
2023-08-04 11:52:39 -0600

    Change file requires to minimum, remove unnessary classes, and move simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'pry'
+require 'csv'
+require 'rspec'
 require './lib/stat_tracker'
-require './lib/team'
-require './lib/season'
-require './lib/game'
-require './lib/game_team'
+# require './lib/season'
+# require './lib/team'
+# require './lib/league'
+# require './lib/game_team'
+# require './lib/season_game_id'
+# require './lib/modules/games_module'
+# require './lib/modules/seasons_module'
+# require './lib/modules/teams_module'
+
