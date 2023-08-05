d6d2960181a0e6861387ffc4c97e0d1d1cf98aa4
Joop Stark <stark.joop@gmail.com>
2023-08-05 14:55:19 -0600

    feat: team_info and it's helper method select_team_object_with

--- lib/modules/teams_module.rb
+++ lib/modules/teams_module.rb
@@ -1,7 +1,23 @@
 require_relative '../team'
+require 'pry'
 
 module Teams
   def count_of_teams
     Team.teams.count
-  end  
+  end 
+  
+  def team_info(team_id)
+     {"team_id" => team_id,
+        "franchise_id" => select_team_object_with(team_id).franchise_id,
+        "team_name" => select_team_object_with(team_id).teamname,
+        "abbreviation" => select_team_object_with(team_id).abbreviation,
+        "link" => select_team_object_with(team_id).link
+      }
+  end
+
+  private
+
+  def select_team_object_with(team_id)
+    Team.teams.find { |team| team.team_id == team_id}
+  end
 end
